30b958b804ad2f1cf4440f99efe8b6ec2ebd5105
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2022-07-24 23:56:51 -0700

    bgan login form creation still need to create a handle submit form handler

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Login() {
+  const [userFormData, setUserFormData] = useState({
+    name:"",
+    lastname:"",
+    password:"",
+    email: "",
+
+  });
+
+  const [register, setRegister] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+
+  function handleFormType(){
+    if(register === true){
+      setRegister(false);
+    } 
+    if(register === false){
+      setRegister(true);
+    }
+  
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <div className='container login-wrapper'>
+    <form className="form-signin">
+    <h1 className="h3 mb-3 font-weight-normal">
+      {register ? 'Register': "Login"}
+    </h1>
+
+    {register ?    <>
+            <input 
+                type="text" 
+                id="name" 
+                name="name"
+                className="form-control mb-3" 
+                placeholder="Your name"
+            />
+
+            <input 
+                type="text" 
+                id="lastname" 
+                name="lastname"
+                className="form-control mb-3" 
+                placeholder="Your lastname"
+            />
+        </>
+      : null  
+      
+      }
+
+    
+
+
+
+    <input 
+        type="email" 
+        id="email" 
+        name="email"
+        className="form-control mb-3" 
+        placeholder="Email address"
+    />
+
+    <input 
+        type="password" 
+        id="password" 
+        name="password"
+        className="form-control" 
+        placeholder="Password" 
+    />
+
+    <br/>
+    <button 
+        className="btn btn-lg btn-primary btn-block" 
+        type="submit"
+        disabled={loading}
+    >
+        {register ? 'Register': "Login"}
+    </button>
+
+    <div className="mt-3">
+         {register ? 'Need to sign in':'Not Registered?'} Click 
+        <span  
+            className="login_type_btn" 
+            onClick={handleFormType}
+        > here</span> to {register ? 'Login.': "Register."}
+    </div>
+</form>
+</div>
+</>
   )
 }
 
